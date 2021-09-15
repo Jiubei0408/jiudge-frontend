61cfb57402c888190b9a1a09f32a7e8791e1341f
Jiubei0408 <757795210@qq.com>
2021-09-15 23:13:23 +0800

    改为https协议

--- src/plugins/store.js
+++ src/plugins/store.js
@@ -5,7 +5,7 @@ Vue.use(Vuex)
 
 const debug = false
 const localhost = false
-let api = 'http://jiudge-api.zuccacm.top/v1'
+let api = 'https://jiudge-api.zuccacm.top/v1'
 
 if (localhost) {
     api = 'http://127.0.0.1:5000/v1'
