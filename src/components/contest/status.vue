306e34ded0639edb76feda6d8b79d7e375eab110
Jiubei0408 <757795210@qq.com>
2022-04-01 16:06:50 +0800

    1.添加了愚人节彩蛋 2.新增running筛选 3.点击题目名跳转到题面

--- src/components/contest/status.vue
+++ src/components/contest/status.vue
@@ -34,12 +34,18 @@
           </el-tooltip>
         </template>
       </el-table-column>
-      <el-table-column label="题目" prop="problem.problem_id" width="60px"/>
+      <el-table-column label="题目" prop="problem.problem_id" width="60px">
+        <template v-slot="scope">
+          <el-link :underline="false" @click="gotoProblem(scope.row.problem.problem_id)">
+            {{ scope.row.problem.problem_id }}
+          </el-link>
+        </template>
+      </el-table-column>
       <el-table-column label="结果" prop="view_result" width="100px">
         <template slot-scope="scope">
           <el-tooltip style="width: fit-content" :content="scope.row.remote_result" placement="top">
             <p style="font-weight: bold" :style="{'color': getResultColor(scope.row)}">
-              {{ scope.row.view_result }}
+              {{ getResultText(scope.row) }}
             </p>
           </el-tooltip>
         </template>
@@ -69,7 +75,7 @@
 import BaseBoxFrame from "@/components/globals/base-box-frame";
 import ViewCodeDialog from "@/components/globals/view-code-dialog";
 
-let judgeResults = ['PENDING', 'AC', 'WA', 'TLE', 'MLE', 'RE', 'CE', 'UNKNOWN', 'SpiderError']
+let judgeResults = ['PENDING', 'RUNNING', 'AC', 'WA', 'TLE', 'MLE', 'RE', 'CE', 'UNKNOWN', 'SpiderError']
 
 export default {
   name: "status",
@@ -102,6 +108,9 @@ export default {
     }
   },
   methods: {
+    gotoProblem(problem_id){
+      this.$router.push(`/contest/${this.contest_id}/problem/${problem_id}`)
+    },
     refreshData() {
       this.loading = true
       this.$http.get(this.api + `/contest/${this.contest_id}/status`, {
@@ -115,7 +124,24 @@ export default {
         this.$message.error(err.response.data.msg)
       })
     },
+    getResultText(submission) {
+      let now = this.$moment()
+      //愚人节彩蛋
+      if (now.month() + 1 === 4 && now.date() === 1) {
+        if (submission.view_result === 'WA') {
+          return 'AC?'
+        }
+      }
+      return submission.view_result
+    },
     getResultColor(submission) {
+      let now = this.$moment()
+      //愚人节彩蛋
+      if (now.month() + 1 === 4 && now.date() === 1) {
+        if (submission.view_result === 'WA') {
+          return '#21ba45'
+        }
+      }
       let result = submission.view_result
       if (result === 'AC') return '#21ba45'
       if (result === 'PENDING' || result === 'RUNNING') return '#c7c7c7'
